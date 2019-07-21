d28bda1097efcec7f245a243526a9fda265e6dce
lucaswiix <lucasrocha.rosaborges@hotmail.com>
2019-07-21 16:35:42 -0300

    feat: userService to find profile by id:Bomb:

---
+++ README.md
@@ -0,0 +1,8 @@
+# spring-boot-security-oauth2
+
+1. Spring Boot
+2. Hibernate
+3. Java 8
+4. MySql
+
+
---
+++ src/main/java/com/wiixzer/service/UserService.java
@@ -0,0 +1,12 @@
+package com.wiixzer.service;
+
+import com.wiixzer.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    User save(User user);
+    List<User> findAll();
+    void delete(long id);
+}
